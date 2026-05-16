ae870eec935fe3d7fdd690d46bb4f710c47b2e3c
Alexandru <lexyus22@gmail.com>
2026-05-16 13:41:41 +0200

    Add battle loop with win condition.

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from character import Character
 player_1 = Character('Player_1', 500, 30)
 enemy_1  = Character('Goblin', 700, 10)
 
-player_1.show_status()
-enemy_1.show_status()
+while player_1.is_alive() and enemy_1.is_alive():
+    print(f'{player_1.name} attack {enemy_1.name}')
+    player_1.attack(enemy_1)
+    enemy_1.show_status()
 
-player_1.attack(enemy_1)
-enemy_1.attack(player_1)
+    print('\n')
 
-print('\n')
+    if enemy_1.is_alive():
+        print(f'{enemy_1.name} attacks {player_1.name}')
+        enemy_1.attack(player_1)
+        player_1.show_status()
 
-player_1.show_status()
-enemy_1.show_status()
+        print('\n')
+
+if player_1.is_alive():
+    print(f'{player_1.name} wins!')
+else:
+    print(f'{enemy_1.name} wins!')
